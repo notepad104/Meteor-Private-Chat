5b0776d06e386cc2a4e2beb95593cc46fbc84a61
Shubham Aggarwal <shubham.iiitm.11@gmail.com>
2015-05-04 13:55:10 +0530

    Limiting Messages and Automatically scroll to bottom on load

--- client/templates/userMessageBox.js
+++ client/templates/userMessageBox.js
@@ -1,8 +1,28 @@
+incrementLimit = function() {
+  newLimit = Session.get('limit') + 10;
+  Session.set('limit', newLimit);
+}
+Template.userMessageBox.created = function(){
+    Session.setDefault('limit', 10);
+}
 Template.userMessageBox.helpers({
     messages: function () {
     	send = Meteor.user().profile.name;
     	receive = Session.get('curuser');
-        return Chat.find({$or : [{$and : [{user: send},{sentTo: receive}]}, {$and : [{user: receive},{sentTo: send}]}]}, {sort: {sentOn: 1}});
+        chats =  Chat.find({
+                            $or : [
+                                    {$and : [{user: send},{sentTo: receive}]}, 
+                                    {$and : [{user: receive},{sentTo: send}]}
+                                ]
+                        }, 
+                        {
+                            sort: {
+                                sentOn: -1
+                            },
+                            limit: Session.get('limit')
+                        }).fetch();
+        chats.sort(function(a,b) { return a.sentOn - b.sentOn } );
+        return chats;
     },
     curuser: function(){
     	return Session.get('curuser');
@@ -11,11 +31,18 @@ Template.userMessageBox.helpers({
 Template.userMessageBox.events({
     'submit #chatMsgForm': function (event, template) {
         event.preventDefault();
+        Session.set('limit', Session.get('limit') + 1);
         var message = template.$('[id=textMessage]').val();
         Meteor.call("newChatMessage", message, Session.get('curuser'));
         template.$('[id=textMessage]').val("");
         var elem = template.$('[id=messages]');
-        console.log(elem[0]);
-        elem.animate({ scrollTop: elem[0].scrollHeight}, 2000);
+        elem.animate({ scrollTop: elem[0].scrollHeight}, 200);
+    },
+    'click #incr': function(e) {
+        incrementLimit();
     }
 });
+Template.userMessageBox.rendered = function(){
+    var elem = $(this.find('[id=messages]'))
+    elem.animate({ scrollTop: elem[0].scrollHeight}, 200);
+}
